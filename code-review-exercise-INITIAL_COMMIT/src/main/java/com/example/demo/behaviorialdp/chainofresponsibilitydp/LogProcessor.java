272497f464b5137a7673e82795849c430cd604f4
ravikp92 <ravikumarpuri04@gmail.com>
2025-01-05 19:57:12 +0530

    more pattern

---
+++ code-review-exercise-INITIAL_COMMIT/src/main/java/com/example/demo/behaviorialdp/chainofresponsibilitydp/LogProcessor.java
@@ -0,0 +1,21 @@
+package com.example.demo.behaviorialdp.chainofresponsibilitydp;
+
+public abstract class LogProcessor {
+    public static int INFO = 1;
+    public static int DEBUG = 1;
+    public static int ERROR = 1;
+
+    LogProcessor nextLogProcessor;
+
+    LogProcessor(LogProcessor logProcessor) {
+        this.nextLogProcessor = logProcessor;
+    }
+
+    public void log(int logLevel, String message) {
+        if (nextLogProcessor != null) {
+            nextLogProcessor.log(logLevel, message);
+        }
+    }
+
+
+}
